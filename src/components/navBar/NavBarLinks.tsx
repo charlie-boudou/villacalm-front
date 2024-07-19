3ab5fabc98cc557a5443fa92f30ce4ce9f0220c1
charlieboudou <charlie.lcb3@gmail.com>
2024-07-19 21:13:20 +0400

    scroll to about

--- src/components/navBar/NavBarLinks.tsx
+++ src/components/navBar/NavBarLinks.tsx
@@ -9,21 +9,21 @@ interface INavBarLinksProps {
 
 function NavBarLinks({ link, value, isHome }: INavBarLinksProps): JSX.Element {
   const location = useLocation();
-  const navigate = useNavigate();
 
   useEffect(() => {
     if (location.pathname === '/' && window.localStorage.getItem('scrollToAbout') === 'true') {
-      const aboutElement = document.getElementById('about');
-      if (aboutElement) {
-        aboutElement.scrollIntoView({ behavior: 'smooth' });
-      }
+      setTimeout(() => {
+        const aboutElement = document.getElementById('about');
+        if (aboutElement) {
+          aboutElement.scrollIntoView({ behavior: 'smooth' });
+        }
+      }, 100);
     }
-  }, [location, window.localStorage.scrollToAbout]);
+  }, [location.pathname]);
 
   const handleClickAbout = () => {
     if (location.pathname !== '/') {
       window.localStorage.setItem('scrollToAbout', 'true');
-      navigate('/');
     } else {
       const aboutElement = document.getElementById('about');
       if (aboutElement) {
@@ -33,24 +33,15 @@ function NavBarLinks({ link, value, isHome }: INavBarLinksProps): JSX.Element {
   };
 
   return (
-    <>
-      {value === 'ABOUT' ? (
-        <div 
-          onClick={handleClickAbout}
-          className={`h-fit w-fit cursor-pointer ${isHome ? 'border-gold' : 'border-white'} ${location.pathname.replace('/', '') === value.toLowerCase() ? 'bg-white text-gold' : 'text-white'} font-bold md:text-[1.5rem] px-[1rem] rounded-[.5rem] border-[.15rem] darlene md:transition md:ease-in-out md:delay-150 md:hover:-translate-y-1 md:hover:scale-110 md:duration-300`}
-        >
-          {value}
-        </div>
-      ) : (
-        <Link 
-          to={link}
-          className={`h-fit w-fit ${isHome ? 'border-gold' : 'border-white'} ${location.pathname.replace('/', '') === value.toLowerCase() ? 'bg-white text-gold' : 'text-white'} font-bold md:text-[1.5rem] px-[1rem] rounded-[.5rem] border-[.15rem] darlene md:transition md:ease-in-out md:delay-150 md:hover:-translate-y-1 md:hover:scale-110 md:duration-300`}
-        >
-          {value}
-        </Link>
-      )}
-    </>
+    <Link 
+      to={link}
+      className={`h-fit w-fit ${isHome ? 'border-gold' : 'border-white'} ${location.pathname.replace('/', '') === value.toLowerCase() ? 'bg-white text-gold' : 'text-white'} font-bold md:text-[1.5rem] px-[1rem] rounded-[.5rem] border-[.15rem] darlene md:transition md:ease-in-out md:delay-150 md:hover:-translate-y-1 md:hover:scale-110 md:duration-300`}
+      onClick={handleClickAbout}
+    >
+      {value}
+    </Link>
   );
 }
 
 export { NavBarLinks };
+
