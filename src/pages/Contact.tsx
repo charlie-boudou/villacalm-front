1512f1298a1e846db90d73f02607e6e2fd72271d
charlieboudou <charlie.lcb3@gmail.com>
2024-07-19 14:41:30 +0400

    clean code

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -4,7 +4,6 @@ import { useSelector } from "react-redux";
 function Contact(): JSX.Element {
   const images = useSelector((state: any) => state.images);
 
-  console.log('rep', images.mockUps[images.mockUps.length - 1]);
   return (
     <div className="p-[1rem] flex flex-col w-full h-screen">
       <img src={images.mockUps[images.mockUps.length - 1].src} alt={images.mockUps[images.mockUps.length - 1].title} />
